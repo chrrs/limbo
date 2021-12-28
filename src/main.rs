c792004aefb8d198e17533d8286cf81b04c29bd5
Chris <chirssel@gmail.com>
2021-12-28 10:39:30 +0100

    Add the cargo project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::net::TcpListener;
+
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:9010").context("failed to open TCP listener")?;
+
+    for _ in listener.incoming() {
+        println!("new connection!")
+    }
+
+    Ok(())
+}
