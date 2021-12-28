6b732403966a22efdd2a473b52c19b4c42e248c3
Chris <chirssel@gmail.com>
2021-12-28 17:19:38 +0100

    Respond to server pings again

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::{
-    io::{Cursor, Read},
+    io::{Cursor, Read, Write},
     net::TcpListener,
 };
 
 use anyhow::{Context, Result};
 use protocol::{Readable, VarInt};
 
-use crate::protocol::packets::{client::ClientPacket, State};
+use crate::protocol::{
+    info::{Motd, PlayerInfo, ServerInfo, VERSION},
+    packets::{
+        client::{handshake::ClientHandshakePacket, status::ClientStatusPacket, ClientPacket},
+        server::{status::ServerStatusPacket, ServerPacket},
+        State,
+    },
+    Writable,
+};
 
 mod protocol;
 
@@ -16,6 +24,7 @@ fn main() -> Result<()> {
     for stream in listener.incoming() {
         let mut stream = stream?;
 
+        let mut state = State::Handshake;
         loop {
             let mut buffer = [0; 5];
             stream
@@ -34,7 +43,41 @@ fn main() -> Result<()> {
             let mut cursor = Cursor::new(&buffer[..]);
             cursor.set_position(length_cursor.position());
 
-            println!("{:?}", ClientPacket::decode(State::Handshake, &mut cursor));
+            let packet = ClientPacket::decode(state, &mut cursor)
+                .context("failed to decode received packet")?;
+            println!("received {:?}", packet);
+
+            match packet {
+                ClientPacket::Handshake(ClientHandshakePacket::Handshake {
+                    next_state, ..
+                }) => state = next_state,
+                ClientPacket::Status(ClientStatusPacket::Request {}) => {
+                    let mut payload = Vec::new();
+
+                    let packet = ServerPacket::Status(ServerStatusPacket::Response {
+                        response: serde_json::to_string(&ServerInfo::new(
+                            VERSION,
+                            PlayerInfo::simple(10, 10),
+                            Motd::new("Limbo".into()),
+                        ))
+                        .context("failed to serialize ping packet")?,
+                    });
+
+                    let mut data = packet
+                        .encode()
+                        .context("failed to encode ping response packet")?;
+                    VarInt(data.len() as i32)
+                        .write_to(&mut payload)
+                        .context("failed to write packet length")?;
+                    payload.append(&mut data);
+
+                    stream
+                        .write_all(&payload)
+                        .context("failed to send packet")?;
+
+                    println!("sent {:?}", packet);
+                }
+            }
         }
     }
 
--- src/protocol/packets/server/mod.rs
+++ src/protocol/packets/server/mod.rs
@@ -1,3 +1,5 @@
+use crate::protocol::{ProtocolError, Writable};
+
 use self::status::ServerStatusPacket;
 
 pub mod status;
@@ -5,4 +7,16 @@ pub mod status;
 #[derive(Debug)]
 pub enum ServerPacket {
     Status(ServerStatusPacket),
+}
+
+impl ServerPacket {
+    pub fn encode(&self) -> Result<Vec<u8>, ProtocolError> {
+        let mut buffer = Vec::new();
+
+        match self {
+            ServerPacket::Status(packet) => packet.write_to(&mut buffer)?,
+        }
+
+        Ok(buffer)
+    }
 }
