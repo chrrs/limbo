ad421cf4a2d33d43c83c98ee5270356b9329e583
Chris <chirssel@gmail.com>
2021-12-28 13:00:17 +0100

    Implement protocol parsing for integers

--- src/protocol/io.rs
+++ src/protocol/io.rs
@@ -1,5 +1,8 @@
 use std::io::{Cursor, Read};
 
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use paste::paste;
+
 use super::{ProtocolError, Readable, VarInt, Writable};
 
 impl Readable for String {
@@ -16,5 +19,51 @@ impl Writable for String {
         VarInt(self.len() as i32).write_to(buffer)?;
         buffer.extend(self.bytes());
         Ok(())
+    }
+}
+
+macro_rules! impl_int {
+    ($($typ:ident),+) => {
+        $(
+            paste! {
+                impl Readable for $typ {
+                    fn read_from(buffer: &mut Cursor<&[u8]>) -> Result<Self, ProtocolError> {
+                        Ok(buffer.[<read_ $typ>]::<BigEndian>()?)
+                    }
+                }
+
+                impl Writable for $typ {
+                    fn write_to(&self, buffer: &mut Vec<u8>) -> Result<(), ProtocolError> {
+                        Ok(buffer.[<write_ $typ>]::<BigEndian>(*self)?)
+                    }
+                }
+            }
+        )+
+    };
+}
+
+impl_int!(u16, u32, u64, i16, i32, i64);
+
+impl Readable for u8 {
+    fn read_from(buffer: &mut Cursor<&[u8]>) -> Result<Self, ProtocolError> {
+        Ok(buffer.read_u8()?)
+    }
+}
+
+impl Writable for u8 {
+    fn write_to(&self, buffer: &mut Vec<u8>) -> Result<(), ProtocolError> {
+        Ok(buffer.write_u8(*self)?)
+    }
+}
+
+impl Readable for i8 {
+    fn read_from(buffer: &mut Cursor<&[u8]>) -> Result<Self, ProtocolError> {
+        Ok(buffer.read_i8()?)
+    }
+}
+
+impl Writable for i8 {
+    fn write_to(&self, buffer: &mut Vec<u8>) -> Result<(), ProtocolError> {
+        Ok(buffer.write_i8(*self)?)
     }
 }
