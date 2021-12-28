53859a2cb13275f9eef52416a502a3c3ccc8f3ff
Chris <chirssel@gmail.com>
2021-12-28 17:22:24 +0100

    Move the packet macro into the protocol module

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -4,6 +4,59 @@ use thiserror::Error;
 
 pub use variable::*;
 
+macro_rules! packet {
+    {
+        $(#[$meta:meta])*
+        $vis:vis enum $name:ident {
+            $(
+                $id:literal = $packet:ident {
+                    $($field:ident: $typ:ident),*
+                    $(,)?
+                }
+            ),*
+            $(,)?
+        }
+    } => {
+        $(#[$meta])*
+        $vis enum $name {
+            $(
+                $packet {
+                    $($field: $typ),*
+                }
+            ),*
+        }
+
+        impl crate::protocol::Readable for $name {
+            fn read_from(buffer: &mut std::io::Cursor<&[u8]>) -> Result<Self, crate::protocol::ProtocolError> {
+                match crate::protocol::VarInt::read_from(buffer)?.0 {
+                    $(
+                        $id => Ok(Self::$packet {
+                            $(
+                                $field: $typ::read_from(buffer)?,
+                            )*
+                        }),
+                    )*
+                    id => Err(crate::protocol::ProtocolError::InvalidPacketId(id)),
+                }
+            }
+        }
+
+        impl crate::protocol::Writable for $name {
+            fn write_to(&self, buffer: &mut Vec<u8>) -> Result<(), crate::protocol::ProtocolError> {
+                match self {
+                    $(
+                        Self::$packet { $($field),* } => {
+                            crate::protocol::VarInt($id).write_to(buffer)?;
+                            $($field.write_to(buffer)?;)*
+                            Ok(())
+                        },
+                    )*
+                }
+            }
+        }
+    };
+}
+
 macro_rules! packet_enum {
     {
         $(#[$meta:meta])*
