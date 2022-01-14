3635af761385d2f97e2fa853f9e8305323b6b5f9
Chris <chirssel@gmail.com>
2022-01-14 14:20:20 +0100

    Add a Position protocol type

--- protocol/src/types.rs
+++ protocol/src/types.rs
@@ -40,3 +40,36 @@ impl Writable for Option<GameMode> {
         }
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+impl Position {
+    pub fn new(x: i32, y: i32, z: i32) -> Position {
+        Position { x, y, z }
+    }
+}
+
+impl Readable for Position {
+    fn read_from(buffer: &mut Cursor<&[u8]>) -> Result<Position, ProtocolError> {
+        let value = u64::read_from(buffer)?;
+        Ok(Position {
+            x: (value >> 38) as i32,
+            y: ((value << 52) >> 52) as i32,
+            z: ((value << 26) >> 38) as i32,
+        })
+    }
+}
+
+impl Writable for Position {
+    fn write_to(&self, buffer: &mut dyn Write) -> Result<(), ProtocolError> {
+        ((((self.x as u64) & 0x3ffffff) << 38)
+            | ((self.y as u64) & 0xfff)
+            | (((self.z as u64) & 0x3ffffff) << 12))
+            .write_to(buffer)
+    }
+}
